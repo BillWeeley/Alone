9852904a271d62afe9a483675d62def3f03bba7f
BillWeeley <61968397+BillWeeley@users.noreply.github.com>
2021-05-20 23:58:31 +0100

    Finished

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public Transform ItemSpawn;
-    public GameObject Tree;
+    
     public int maxHealth = 100;
     int currentHealth;
     void Start()
@@ -20,28 +19,16 @@ public class Enemy : MonoBehaviour
 
         if(currentHealth <= 0)
         {
-            DropItems();
             Die();
         }
     }
 
     void Die()
     {
-        Destroy(ItemSpawn.gameObject);  
+        Destroy(gameObject);  
         
     }
 
-    private void Instantiate(GameObject item, Vector3 position)
-    {
-        throw new NotImplementedException();
+    
     }
 
-    void DropItems()
-    {
-        Transform objectToDrop;
-        if (ItemSpawn.gameObject.CompareTag ("Trees"))
-        {
-            Instantiate(Tree, ItemSpawn);
-        }
-    }
-}
